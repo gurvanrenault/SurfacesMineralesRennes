eb479777157b9127ee0cb0782f4df6648594b4f2
= <renault.gurvan@gmail.com>
2020-05-12 16:17:18 +0200

    ***  CSS/HTML - Ajout de Boostrap - Menu - Sélection surface - Nettoyage de l'interface principale *** JS - Affichage de plusieurs polygones - Création d'un controlleur pour les possiblités de séléction de surfaces

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,18 @@ var app = angular.module('app', ['ngMap']);
 app.controller('map', function($scope) {
   $scope.sometext= "";
   $scope.coulcontour = "#FF0000";
-  $scope.chemins = "[[25.774252, -85.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
-  
-  $scope.couleurinterieur = "#FF0000";
-  $scope.opacitecontour ="0.8";
-  $scope.opaciteinterieur ="0.35";
+  $scope.polygons= {}
+  $scope.polygons[0] = new Object()
+  $scope.polygons[0].chemin = "[[25.774252, -85.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
+  $scope.polygons[0].couleurinterieur = "#FF0000";
+  $scope.polygons[0].opacitecontour ="0.8";
+  $scope.polygons[0].opaciteinterieur ="0.35";
+  $scope.polygons[1] = new Object();
+  $scope.polygons[1].chemin = "[[25.774252, -100.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
+  $scope.polygons[1].couleurinterieur = "#FF0000";
+  $scope.polygons[1].opacitecontour ="0.8";
+  $scope.polygons[1].opaciteinterieur ="0.35";
+});
+app.controller('selector', function($scope) {
+    $scope.nom_polygone=["Select 1 ","Select 2","Select 3"]
 });
