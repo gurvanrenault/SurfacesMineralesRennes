16355bdc79bf4c32d0318f39d3fbdaebaf76638f
AlexDjango29 <49725178+AlexDjango29@users.noreply.github.com>
2020-05-12 23:40:10 +0200

    Ajax request + surfaces drawing
    Envoi de la requete ajax + parsing des données reçues
    Modification dans l'html pour correspondre au nouveau nom de la variable
    Modification du zoom et center position de départ pour correspondre au jardin du thabor
    Mise en commentaire des shapes du triangle des bermudes (supprime si t'as plus besoin)

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,56 @@
 var app = angular.module('app', ['ngMap']);
-app.controller('map', function($scope) {
-  $scope.sometext= "";
-  $scope.coulcontour = "#FF0000";
-  $scope.polygons= {}
-  $scope.polygons[0] = new Object()
-  $scope.polygons[0].chemin = "[[25.774252, -85.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
-  $scope.polygons[0].couleurinterieur = "#FF0000";
-  $scope.polygons[0].opacitecontour ="0.8";
-  $scope.polygons[0].opaciteinterieur ="0.35";
-  $scope.polygons[1] = new Object();
-  $scope.polygons[1].chemin = "[[25.774252, -100.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
-  $scope.polygons[1].couleurinterieur = "#FF0000";
-  $scope.polygons[1].opacitecontour ="0.8";
-  $scope.polygons[1].opaciteinterieur ="0.35";
+
+// Controleur de la carte
+app.controller('map', function($scope, $http) {
+
+    // Requête ajax récupérant les polygones représentants les surfaces minérales
+    $http.get("https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=surfaces-minerales-du-jardin-du-thabor")
+        // En cas de succès (code retour = 200)
+        .success(function(data){
+
+            // On récupère le nombre de surfaces
+            var nb_surfaces = data.parameters.rows;
+
+            // On crée un dictionnairequi va contenir toutes les surfaces
+            $scope.surfaces = {};
+
+            // On crée le dictionnarie contenant les polygones et leurs paramètres
+
+            for(var i=0; i<nb_surfaces; i++){
+                // Création de la nouvelle surface
+                $scope.surfaces[i] = {};
+
+                // Création du chemin
+                var chemin = data.records[i].fields.geo_shape.coordinates[0];
+                $scope.surfaces[i].chemin = [];
+                for(var j=0; j<chemin.length; j++){
+                    $scope.surfaces[i].chemin.push([chemin[j][1], chemin[j][0]])
+                }
+                $scope.surfaces[i].centre = data.records[i].geometry.coordinates;
+                $scope.surfaces[i].couleurinterieur = "#FF0000";
+                $scope.surfaces[i].opacitecontour = "0.8";
+                $scope.surfaces[i].opaciteinterieur = "0.35";
+            }
+            console.log($scope.surfaces);
+        });
+
+    // $scope.sometext= "";
+    // $scope.coulcontour = "#FF0000";
+    // $scope.polygons= {}
+    // $scope.polygons[0] = new Object()
+    // $scope.polygons[0].chemin = "[[25.774252, -85.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
+    // $scope.polygons[0].couleurinterieur = "#FF0000";
+    // $scope.polygons[0].opacitecontour ="0.8";
+    // $scope.polygons[0].opaciteinterieur ="0.35";
+    // $scope.polygons[1] = new Object();
+    // $scope.polygons[1].chemin = "[[25.774252, -100.190262],[18.466465, -66.118292],[32.321384, -64.75737],[25.774252, -80.190262]]";
+    // $scope.polygons[1].couleurinterieur = "#FF0000";
+    // $scope.polygons[1].opacitecontour ="0.8";
+    // $scope.polygons[1].opaciteinterieur ="0.35";
 });
+
+// Controleur pour les sélecteurs de la carte
 app.controller('selector', function($scope) {
     $scope.nom_polygone=["Select 1 ","Select 2","Select 3"]
 });
+
